9898f67876a9d5a0479feaf0cd87752e4531fe70
FilipFekete <fifek23@student.sdu.dk>
2024-02-05 06:06:40 +0100

    Created Tournament Viewer form

---
+++ TrackerUI/TournamentViewerForm.cs
@@ -0,0 +1,25 @@
+namespace TrackerUI
+{
+    public partial class TournamentViewerForm : Form
+    {
+        public TournamentViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scoreButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
